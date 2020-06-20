f699fe018be00cb67ce9ff7127f4307e744e0e31
Ekaterina-Ledneva <meikberri@gmail.com>
2020-06-20 21:28:52 +0300

    Changes on IDE Intellij Idea

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("Next Commit");
+	System.out.println("Changes on IDE Intellij Idea");
 	// write your code here
     }
 }
